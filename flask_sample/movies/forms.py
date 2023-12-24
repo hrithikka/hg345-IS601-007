29ebfb7ef534bd8bf5cb548f6d636bb618a79aad
Hrithikka <hrithikahone@gmail.com>
2023-12-23 22:03:28 -0500

    push

--- flask_sample/movies/forms.py
+++ flask_sample/movies/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, DateField, TextAreaField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, URL, Length, Optional, NumberRange, EqualTo
+from wtforms.validators import DataRequired, URL, Length, Optional, NumberRange, EqualTo,ValidationError
+# Custom validator for genre
+def validate_genre(form, field):
+    if field.data != "horror":
+        raise ValidationError("Genre must be 'horror'")
 
 class MovieForm(FlaskForm):
     imageURL = StringField("Image URL", validators=[DataRequired(), URL(message="Invalid URL format")])
-    genre = StringField("Genre", validators=[DataRequired(), Length(max=10), EqualTo('horror', message="Genre must be 'horror'")])
+    genre = StringField("Genre", validators=[DataRequired(), Length(max=10), validate_genre])
     title = StringField("Title", validators=[DataRequired(), Length(max=255)])
-    imdbrating = DecimalField("IMDb Rating", validators=[DataRequired(), NumberRange(min=0, max=10, message="Rating must be between 0 and 10")])
+    imdbrating = DecimalField("IMDb Rating", validators=[DataRequired()])
     released = IntegerField("Released Year", validators=[DataRequired(), NumberRange(min=1800, max=2100, message="Enter a valid release year")])
     type = StringField("Type", validators=[DataRequired(), Length(max=20)])
     synopsis = TextAreaField("Synopsis", validators=[DataRequired(), Length(max=1000, message="Synopsis must be 1000 characters or less")])
